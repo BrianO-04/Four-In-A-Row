4f4fcecc43f53a4561838f9d8958c8126e6a4ae8
Brian Olson <brianolson04@gmail.com>
2026-07-10 14:48:00 -0500

    Started filling out board functions

--- include/board.hpp
+++ include/board.hpp
@@ -4,12 +4,15 @@
 class Board {
     private:
         char* positions;
+        char w, h;
+        char getPieceAt(int row, int col);
     public:
         Board(char w, char h);
         ~Board();
         int put(char piece, char col);
         bool check(char row, char col);
         void reset();
+        void printBoard();
 };
 
 #endif // BOARD_H_
--- src/board.cpp
+++ src/board.cpp
@@ -3,7 +3,12 @@
 
 // Create a new board of size w x h
 Board::Board(char w, char h){
+    this->w = w;
+    this->h = h;
     positions = (char*)malloc(sizeof(char) * w * h);
+    for(int i = 0; i < w * h; i++){
+        positions[i] = 0;
+    }
 }
 
 Board::~Board(){
@@ -11,16 +16,50 @@ Board::~Board(){
 }
 
 // Insert a piece in the lowest possible row of a column
+// Returns 1 if a piece cannot be placed
 int Board::put(char piece, char col){
+    if(col < 0 || col > w){
+        return 1;
+    }
+
+    int row = h-1;
+    char pieceAtPos = getPieceAt(row, col);
+    while(pieceAtPos != 0){
+        row -=1;
+        if(row < 0) return 1;
+        pieceAtPos = getPieceAt(row, col);
+    }
+
+    int finalPos = (row * w) + col;
+    positions[finalPos] = piece;
     return 0;
 }
 
 // Check if a piece is part of a winning combination
 bool Board::check(char row, char col){
+    if(row < 0 || row > h || col < 0 || col > w){
+        return 1;
+    }
+
     return 0;
 }
 
 // Reset board
 void Board::reset(){
-    
+    for(int i = 0; i < w * h; i++){
+        positions[i] = 0;
+    }
+}
+
+void Board::printBoard(){
+
+}
+
+char Board::getPieceAt(int row, int col){
+    if(row < 0 || row > h || col < 0 || col > w){
+        return -1;
+    }
+
+    int index = (row * w) + col;
+    return positions[index];
 }
