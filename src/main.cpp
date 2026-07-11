c59d7be6a3de1a242f42c9c8fe44657646b6a377
Brian Olson <brianolson04@gmail.com>
2026-07-11 14:40:58 -0500

    Created main game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,50 @@
 #include <iostream>
+#include <ostream>
 #include "../include/board.hpp"
 
 int main(){
     Board b = Board(6,6);
 
-    b.put(1, 3);
-    b.put(1, 3);
-    b.put(1, 3);
-    b.put(2, 3);
-
-    b.put(1, 2);
-    b.put(1, 2);
-    b.put(2, 2);
-
-    b.put(1, 1);
-    b.put(2, 1);
-
-    b.put(2, 0);
-    
     b.printBoard();
 
-    std::cout << b.check(2, 3) << std::endl;
-    std::cout << b.check(5, 0) << std::endl;
+    while(1){
+        // Player 1 input
+        std::cout << "Player 1, input column: ";
+        int col;
+        std::cin >> col;
+
+        int row = b.put(1, col);
+        while(row == -1){ // Invalid input loop
+            b.printBoard();
+            std::cout << "Invalid input!" << std::endl << "Player 1, input column: ";
+            std::cin >> col;
+            row = b.put(1, col);
+        }
+        b.printBoard();
+        // Player 1 win check
+        if(b.check(row, col)){
+            std::cout << "Player 1 win!" << std::endl;
+            break;
+        }
+
+        // Player 2 input
+        std::cout << "Player 2, input column: ";
+        std::cin >> col;
+
+        row = b.put(2, col);
+        while(row == -1){ // Invalid input loop
+            b.printBoard();
+            std::cout << "Invalid input!" << std::endl << "Player 2, input column: ";
+            std::cin >> col;
+            row = b.put(2, col);
+        }
+        b.printBoard();
+        // Player 2 win check
+        if(b.check(row, col)){
+            std::cout << "Player 2 win!" << std::endl;
+            break;
+        }
+    }
 
     return 0;
 }
