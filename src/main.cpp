bf93faf2f2122f118b32c7e6dc795b2940bf0ae4
Brian Olson <brianolson04@gmail.com>
2026-07-10 20:12:09 -0500

    Added temporary tests for vertical and horizontal win checking

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,23 @@
 #include "../include/board.hpp"
 
 int main(){
-    Board b = Board(4,4);
+    Board b = Board(6,6);
+
+    for(int i = 0; i < 4; i ++){
+        b.put(1, i);
+    }
+    for(int i = 0; i < 3; i ++){
+        b.put(1, 2);
+    }
+    
     b.printBoard();
+
+    std::cout << b.check(5, 0) << std::endl;
+    std::cout << b.check(5, 1) << std::endl;
+    std::cout << b.check(5, 2) << std::endl;
+    std::cout << b.check(5, 3) << std::endl;
+    std::cout << b.check(2, 2) << std::endl;
+    std::cout << b.check(5, 2) << std::endl;
 
     return 0;
 }
