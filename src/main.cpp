06e79140eef1ab170a86441d4ec9c526d50cae96
Brian Olson <brianolson04@gmail.com>
2026-07-10 20:55:50 -0500

    Added temporary test for diagonal check testing

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,24 @@
 int main(){
     Board b = Board(6,6);
 
-    for(int i = 0; i < 4; i ++){
-        b.put(1, i);
-    }
-    for(int i = 0; i < 3; i ++){
-        b.put(1, 2);
-    }
+    b.put(1, 3);
+    b.put(1, 3);
+    b.put(1, 3);
+    b.put(2, 3);
+
+    b.put(1, 2);
+    b.put(1, 2);
+    b.put(2, 2);
+
+    b.put(1, 1);
+    b.put(2, 1);
+
+    b.put(2, 0);
     
     b.printBoard();
 
+    std::cout << b.check(2, 3) << std::endl;
     std::cout << b.check(5, 0) << std::endl;
-    std::cout << b.check(5, 1) << std::endl;
-    std::cout << b.check(5, 2) << std::endl;
-    std::cout << b.check(5, 3) << std::endl;
-    std::cout << b.check(2, 2) << std::endl;
-    std::cout << b.check(5, 2) << std::endl;
 
     return 0;
 }
