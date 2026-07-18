c393c54670dfc329231cf5c1df2b347a515efbbb
Brian Olson <brianolson04@gmail.com>
2026-07-18 14:28:26 -0500

    Cleaned up starting sequence

--- src/main.cpp
+++ src/main.cpp
@@ -8,38 +8,23 @@
 int main(){
     Board* b = new Board(7,6);
 
-    Controller* p1;
+    Controller* p1 = new Player(b, '2', 2);
     Controller* p2;
 
-    std::cout << "Select player 1 type" << std::endl;
-    std::cout << "(Player 2 is always player controlled)" << std::endl;
+    std::cout << "Select player 2 type" << std::endl;
+    std::cout << "(Player 1 is always player controlled)" << std::endl;
     std::cout << "1) Player controlled (Default)" << std::endl;
     std::cout << "2) Random columns" << std::endl;
     std::cout << "3) Simple AI (Recommended)" << std::endl;
     std::cout << "Input: ";
-    int p1Choice = getNumberInput();
-    if(p1Choice == 2){
-        p1 = new RandomComp(b, '1', 1);
-    }else if(p1Choice == 3){
-        p1 = new SimpleComp(b, '1', 1);
+    int pChoice = getNumberInput();
+    if(pChoice == 2){
+        p2 = new RandomComp(b, '1', 1);
+    }else if(pChoice == 3){
+        p2 = new SimpleComp(b, '1', 1);
     }
     else{
-        p1 = new Player(b, '1', 1);
-    }
-
-    // std::cout << "Select player 2 type" << std::endl;
-    // std::cout << "1) Player controlled (Default)" << std::endl;
-    // std::cout << "2) Random columns" << std::endl;
-    // std::cout << "3) Simple AI" << std::endl;
-    // std::cout << "Input: ";
-    // int p2Choice = getNumberInput();
-    int p2Choice = 3;
-    if(p2Choice == 2){
-        p2 = new RandomComp(b, '2', 2);
-    }else if(p2Choice == 3){
-        p2 = new SimpleComp(b, '2', 2);
-    }else{
-        p2 = new Player(b, '2', 2);
+        p2 = new Player(b, '1', 1);
     }
 
     Data* game = new Data();
